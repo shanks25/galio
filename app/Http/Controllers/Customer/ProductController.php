40b1a97a5e1ac9850a0329c591ddf89cad369d62
Amer Sayyed <amer.s@exceptionaire.co>
2020-08-23 23:27:39 +0530

    product detail

--- app/Http/Controllers/Customer/ProductController.php
+++ app/Http/Controllers/Customer/ProductController.php
@@ -10,13 +10,16 @@ class ProductController extends Controller
 {
     //for quick view
     public function quick_view(Request $request)
-	{
-        $product_id  =$request->id; 
-        $latestProduct=ProductMaster::with('productImages')->where('id',$product_id)->first();
+    {
+        $product_id = $request->id;
+        $latestProduct = ProductMaster::with('productImages')->where('id', $product_id)->first();
         $view = view('customer.product.quick_view', compact('latestProduct'))->render();
-				return response()->json(['html' => $view]);
-        
-
-		
-	}
+        return response()->json(['html' => $view]);
+    }
+    public function productDetails($product_id)
+    {
+        $product = ProductMaster::with('productImages', 'keyValues')->where('id', $product_id)->first();
+        // $view = ->render();
+        return view('customer.product.detail', compact('product'));
+    }
 }
