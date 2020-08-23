992088f34ccdcd41996bd59f3ccce834625d814e
Vivek Nandanwar <vivek.n@exceptionaire.co>
2020-08-23 15:57:33 +0530

    test

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Category;
+use App\SubCategory;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -23,6 +25,16 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        
+        view()->composer('*', function ($view) {
+
+            $view->with([
+
+                'CategoryMaster' =>  Category::with('subCategiries')->get(),
+                'SubcategoryMaster'     =>  SubCategory::all(),
+                
+
+            ]);
+        });
     }
 }
